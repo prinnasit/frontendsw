6317cf62d0e81a428a10f5859f0163cb60301f22
prinnasit <prinnasit@gmail.com>
2024-06-26 00:21:21 +0700

    chang next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env : {
-        FRONTEND_URL : process.env.FRONTEND_URL,
-        BACKEND_URL : process.env.BACKEND_URL
-    },
+    // env : {
+    //     FRONTEND_URL : process.env.FRONTEND_URL,
+    //     BACKEND_URL : process.env.BACKEND_URL
+    // },
     images: {
         domains: ['drive.google.com']
     },
